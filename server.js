4bfbd20a3832eab2fa43e8b7ad5270e002cfb9fb
isabellaaston <isabella.aston8@gmail.com>
2020-10-23 14:09:54 +0100

    functionality now exists

--- server.js
+++ server.js
@@ -12,9 +12,13 @@ const handlebars = expressHandlebars({
 })
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.engine('handlebars', handlebars)
 app.set('view engine', 'handlebars')
 
+// Get all restaurants
 app.get('/', async(req, res) => {
     const restaurants = await Restaurant.findAll({
         include: [{model: Menu, as: 'menus'}],
@@ -23,19 +27,111 @@ app.get('/', async(req, res) => {
     res.render('home', {restaurants})
 })
 
-app.get('/:name', async(req, res) => {
-    const restaurant = await Restaurant.findOne({where: {name: req.params.name}})
+// Add a restaurant
+app.post('/', async(req, res) => {
+    await Restaurant.create(req.body)
+    res.redirect('/')
+})
+
+// Get sepcific restaurant by id
+app.get('/:restaurantId', async(req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
     const menus = await restaurant.getMenus( {
         include: [{model: Item, as: 'items'}],
-          nest: true  
-      })
+        nest: true  
+    })
     res.render('restaurant', {restaurant, menus})
 })
 
-app.post('/:name/:id/add', async(req, res) => {
-    const restaurant = await Restaurant.findOne({where: {name: req.params.name}})
-    const menus = await Menu.findByPk(req.params.id)
-    
+// Delete restaurant by id
+app.get('/:restaurantId/delete', async(req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    restaurant.destroy()
+    res.redirect('/')
+})
+
+// Edit a restaurant by id
+app.get('/:restaurantId/update', async(req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    res.render('restaurantEdit', {restaurant})
+})
+app.post('/:restaurantId/update', async(req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    restaurant.update(req.body)
+    const menus = await restaurant.getMenus( {
+        include: [{model: Item, as: 'items'}],
+        nest: true  
+    })
+    res.render('restaurant', {restaurant, menus})
+})
+
+// Add a menu to a restaurant
+app.post('/:restaurantId', async(req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    const menu = await Menu.create(req.body)
+    restaurant.addMenu(menu)
+    const menus = await restaurant.getMenus( {
+        include: [{model: Item, as: 'items'}],
+        nest: true  
+    })
+    res.render('restaurant', {restaurant, menus})
+})
+
+// Edit a menu by id from a restaurant
+app.get('/:restaurantId/:menuId/update', async(req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    const menu = await Menu.findByPk(req.params.menuId)
+    res.render('menuEdit', {restaurant, menu})
+})
+app.post('/:restaurantId/:menuId/update', async(req, res) => {
+    const menu = await Menu.findByPk(req.params.menuId)
+    menu.update(req.body)
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    const menus = await restaurant.getMenus( {
+        include: [{model: Item, as: 'items'}],
+        nest: true  
+    })
+    res.render('restaurant', {restaurant, menus})
+})
+
+// Delete a menu by id from a restaurant
+app.get('/:restaurantId/:menuId/delete', async(req, res) => {
+    const menu = await Menu.findByPk(req.params.menuId)
+    menu.destroy()
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    const menus = await restaurant.getMenus( {
+        include: [{model: Item, as: 'items'}],
+        nest: true  
+    })
+    res.render('restaurant', {restaurant, menus})
+})
+
+// Add an item to a specific menu
+app.get('/:restaurantId/:menuId', async(req, res) => {
+    const menu = await Menu.findByPk(req.params.menuId)
+    res.render('menuAdd', {menu})
+})
+app.post('/:restaurantId/:menuId/', async(req, res) => {
+    const menu = await Menu.findByPk(req.params.menuId)
+    const item = await Item.create(req.body)
+    menu.addItem(item)
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    const menus = await restaurant.getMenus({
+        include: [{model: Item, as: 'items'}],
+        nest: true  
+    })
+    res.render('restaurant', {restaurant, menus})
+})
+
+// Delete an item by id from a menu
+app.get('/:restaurantId/:menuId/:itemId/delete', async(req, res) => {
+    const item = await Item.findByPk(req.params.itemId)
+    item.destroy()
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    const menus = await restaurant.getMenus( {
+        include: [{model: Item, as: 'items'}],
+        nest: true  
+    })
     res.render('restaurant', {restaurant, menus})
 })
 
